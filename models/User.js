ab459416707e6d446a2abf2e7fbb5a9dd92201ce
Nadia Algallaf <nadiaalgallaf164@gmail.com>
2026-08-24 10:51:25 +0300

    update User model

--- models/User.js
+++ models/User.js
@@ -1,28 +1,59 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose'
+import validator from 'validator'
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
+    firstName: {
       type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase:true
+      required: [true, 'First name is required'],
+      trim: true
     },
+
+    lastName: {
+      type: String,
+      required: [true, 'Last name is required'],
+      trim: true
+    },
+
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: {
+        validator: validator.isEmail,
+        message: 'Please provide a valid email address'
+      }
+    },
+
     hashedPassword: {
       type: String,
       required: true
     },
-  },
-  { timestamps: true },
-);
 
-userSchema.set("toJSON", {
+    role: {
+      type: String,
+      enum: {
+        values: ['User', 'Pharmacy']
+      },
+      required: true
+    },
+
+    pharmacyName: {
+      type: String,
+      required: [true, 'Pharmacy name is required']
+    }
+  },
+  { timestamps: true }
+)
+
+userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    delete returnedObject.hashedPassword;
-  },
-});
+    delete returnedObject.hashedPassword
+  }
+})
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User
