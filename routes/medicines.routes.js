87e9a7977a4ac007ed121418b6dccb17497ba595
Shaikha Subah <shaikhama127@gmail.com>
2026-08-26 16:00:27 +0300

    complete medicine CRUD

--- routes/medicines.routes.js
+++ routes/medicines.routes.js
@@ -3,8 +3,10 @@ const router = require("express").Router();
 const verifyToken = require("../middleware/verifyToken");
 const medicinesController = require("../controllers/medicines.controller");
 
-router.get("/", medicinesController.getAllMedicines);
-
-router.post("/", verifyToken, medicinesController.createMedicine);
+router.get("/", medicinesController.getAllMedicines)
+router.get("/:id", medicinesController.getMedicineById)
+router.post("/", verifyToken, medicinesController.createMedicine)
+router.patch("/:id", verifyToken,medicinesController.updateMedicine)
+router.delete("/:id",verifyToken,medicinesController.deleteMedicine)
 
 module.exports = router;
