653392885bcf63d24005a5abf78c329ec0011cbf
Shaikha Subah <shaikhama127@gmail.com>
2026-08-27 14:11:30 +0300

    protect medicine management

--- routes/medicines.routes.js
+++ routes/medicines.routes.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
+const authorizeRole = require("../middleware/authorizeRole")
 
 const verifyToken = require("../middleware/verifyToken");
 const medicinesController = require("../controllers/medicines.controller");
 
 router.get("/", medicinesController.getAllMedicines)
 router.get("/:id", medicinesController.getMedicineById)
-router.post("/", verifyToken, medicinesController.createMedicine)
-router.patch("/:id", verifyToken,medicinesController.updateMedicine)
-router.delete("/:id",verifyToken,medicinesController.deleteMedicine)
+router.post("/", verifyToken,authorizeRole("Pharmacy"), medicinesController.createMedicine)
+router.patch("/:id", verifyToken,authorizeRole("Pharmacy"),medicinesController.updateMedicine)
+router.delete("/:id",verifyToken,authorizeRole("Pharmacy"),medicinesController.deleteMedicine)
 
 module.exports = router;
