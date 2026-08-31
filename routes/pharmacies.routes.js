054785b9bddfb3215cb9fc26fb128a300e91892d
Nadia Algallaf <nadiaalgallaf164@gmail.com>
2026-08-31 22:14:08 +0300

    add pharmacy image upload using upload image middleware

--- routes/pharmacies.routes.js
+++ routes/pharmacies.routes.js
@@ -1,18 +1,36 @@
-const router = require("express").Router()
+const router = require('express').Router()
 
-const authorizeRole = require("../middleware/authorizeRole")
-const verifyToken = require("../middleware/verifyToken")
+const authorizeRole = require('../middleware/authorizeRole')
+const verifyToken = require('../middleware/verifyToken')
+const uploadImage = require('../middleware/uploadImage')
 
-const pharmaciesController = require("../controllers/pharmacies.controller")
+const pharmaciesController = require('../controllers/pharmacies.controller')
 
-router.get("/", pharmaciesController.getAllPharmacies)
+router.get('/', pharmaciesController.getAllPharmacies)
 
-router.get("/:id", pharmaciesController.getPharmacyById)
+router.get('/:id', pharmaciesController.getPharmacyById)
 
-router.post("/",verifyToken,authorizeRole("Pharmacy"),pharmaciesController.createPharmacy)
+router.post(
+  '/',
+  verifyToken,
+  authorizeRole('Pharmacy'),
+  uploadImage('pharmacies').single('pharmacyImg'),
+  pharmaciesController.createPharmacy
+)
 
-router.patch("/:id",verifyToken,authorizeRole("Pharmacy"),pharmaciesController.updatePharmacy)
+router.patch(
+  '/:id',
+  verifyToken,
+  authorizeRole('Pharmacy'),
+  uploadImage('pharmacies').single('pharmacyImg'),
+  pharmaciesController.updatePharmacy
+)
 
-router.delete( "/:id", verifyToken,authorizeRole("Pharmacy"),pharmaciesController.deletePharmacy)
+router.delete(
+  '/:id',
+  verifyToken,
+  authorizeRole('Pharmacy'),
+  pharmaciesController.deletePharmacy
+)
 
 module.exports = router
